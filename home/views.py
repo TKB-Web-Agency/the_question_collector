fee72ffa36bfed91e85d311359ac346c988707e5
brenton <brentonbeltrami@protonmail.com>
2021-05-12 17:22:15 -0500

    added question models

--- home/views.py
+++ home/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Question
 
 # Create your views here.
 def homepage(request):
     return render(request, 'home/homepage.html')
+
+class Questions(generic.ListView):
+    queryset = Question.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return super(generic.ListView, self).get(request, *args, **kwargs)
+
+    template_name = 'home/homepage.html'
