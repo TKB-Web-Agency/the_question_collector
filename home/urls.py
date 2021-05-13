8b08e3880c8ee6891faaca4f18ab0c1a599f62f0
brenton <brentonbeltrami@protonmail.com>
2021-05-12 21:58:43 -0500

    created page & form to add new questions

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ app_name = 'home'
 urlpatterns = [
         path('', views.Questions.as_view(), name='homepage'),
         path('<slug:slug>/', views.QuestionDetail.as_view(), name='question_detail'),
+        path('add-new-question', views.add_new_question, name='add_new_question'),
     ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.views import generic
 from .models import Question
+from .forms import QuestionForm
+import datetime
+from django.utils.text import slugify
 
 # Create your views here.
 def homepage(request):
@@ -21,3 +25,21 @@ class QuestionDetail(generic.DetailView):
         return super(generic.DetailView, self).get(request, *args, **kwargs)
 
     template_name = 'home/question_detail.html'
+
+def add_new_question(request):
+    if request.method == 'POST':
+        form =  QuestionForm(request.POST)
+        if form.is_valid():
+            question_title = form.cleaned_data['question']
+            slug = slugify(question_title)
+            new_item = Question.objects.create(title=question_title, slug=slug, date_created=datetime.datetime.now())
+            new_item.save()
+            print(new_item)
+            return HttpResponseRedirect('/')
+
+    else:
+        form = QuestionForm()
+
+    return render(request, 'home/add_new_question.html', {'form': form})
+
+
